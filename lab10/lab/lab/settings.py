2f22968fb03064474916adc101923d0c70ffb49b
chaitra1403 <58132646+chaitra1403@users.noreply.github.com>
2021-07-23 20:49:53 +0530

    Update settings.py

--- lab10/lab/lab/settings.py
+++ lab10/lab/lab/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g&lj7-o!f71pmu%1v-q04m@k2u6=sfe=-&_e4jfe+jmubm!q_f'
+SECRET_KEY = 'pasteYourKeyHere'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
